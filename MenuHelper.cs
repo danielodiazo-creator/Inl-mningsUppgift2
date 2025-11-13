c15aa9ab8794dfca2e805ffc3c37fcdb56ea389b
danielodiazo-creator <danielodiazo@gmail.com>
2025-11-13 08:04:14 +0100

    Skapar en metod Menu inuti klassen MenuHelper

--- MenuHelper.cs
+++ MenuHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,39 @@ namespace InlämningsUppgift2
 {
     public class MenuHelper
     {
-       
+        public void Menu()
+        {
+            AnsiConsole.MarkupLine("[blue bold] Hej och välkommen [/]");
+
+            string option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Välj Alternativ")
+                .AddChoices("Skapa ny användare", "Logga in", "Lägga till order", "Spåra order")      
+                );
+
+            switch (option)
+            {
+                case "Skapa ny användare":
+                    User.CreateNewUser();
+                    break;
+
+                case "Logga in":
+                    
+                    break;
+
+                case "Lägg till order":
+
+                    break;
+
+                case "Spåra order":
+
+                    break;
+
+            }
+
+
+
+        }
         
 
 
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MenuHelper menu = new MenuHelper();
+            menu.Menu();
         }
     }
 }
