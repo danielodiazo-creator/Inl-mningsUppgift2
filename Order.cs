76664a99eef704a7479d131f70fa1f38ef88d65e
danielodiazo-creator <danielodiazo@gmail.com>
2025-11-23 13:16:36 +0100

    Jag har addat metoden showOrderHistory i klassen order så att man kan kolla på ett register av alla köp

--- Order.cs
+++ Order.cs
@@ -9,8 +9,23 @@ namespace InlämningsUppgift2
 {
     public class Order
     {
-       
+        List<Product> Products { get; set; }
+        public double Total { get; set; }
+        public DateTime OrderDate {get; set;}
 
+        public static List<Order> ListOfOrders { get; set; } = new List<Order>();
+
+        public void AddOrder(List<Product> products)
+        {
+            var newOrder = new Order
+            {
+                Products = new List<Product>(products),
+                Total = products.Sum(p => p.price),
+                OrderDate = DateTime.Now,
+            };
+
+            ListOfOrders.Add(newOrder);
+        }
 
         public void CheckOut()
         {
@@ -24,19 +39,49 @@ namespace InlämningsUppgift2
 
             if (choice == "Ja")
             {
+                
+                
                 double total = Product.Total();
                 AnsiConsole.MarkupLine($"[bold blue] Total köpt: {Product.Total()} [/]");
+
+                AddOrder(Product.shoppingCar);
                 Product.shoppingCar.Clear();
 
             }
             else
             {
-                AnsiConsole.MarkupLine("[bold red] Köp avbruten [/]");
+                AnsiConsole.MarkupLine("[bold red] Köp avbrutet [/]");
             }
 
         }
 
-        
+        public static void ShowOrderHistory()
+        {
+            if(Order.ListOfOrders.Count == 0)
+            {
+                AnsiConsole.MarkupLine("[bold red]Inga tidigare köp hittades[/]");
+                return;
+
+            }
+
+            else
+            {
+                foreach(var x in Order.ListOfOrders)
+                {
+                    AnsiConsole.MarkupLine($"[bold green] Order från {x.OrderDate} [/]");
+
+                    foreach(var p in x.Products)
+                    {
+                        AnsiConsole.MarkupLine($"{p.name} : {p.price}");
+                    }
+
+                    AnsiConsole.MarkupLine($"[bold yellow] Total: {x.Total}[/]");
+
+
+                }
+            }
+
+        }
 
     }
 }
